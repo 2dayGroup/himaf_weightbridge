b3a5860c3a49a9005caddcb81f3f4e45e27599bf
AFFECHI Achoué Marc Hilarion <marc_affechi@hotmail.com>
2023-07-17 18:47:04 +0000

    add channel

--- models/weightbridge_raw_material.py
+++ models/weightbridge_raw_material.py
@@ -4,6 +4,7 @@ from odoo import api, fields, models, _
 class WeightbridgeRawMaterial(models.Model):
     _name = "weightbridge.raw.material"
     _description = "Raw Material purchase from weightbridge"
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     
     
     name = fields.Char('Ticket Reference', required=True, index='trigram', copy=False, default='New')
@@ -22,4 +23,5 @@ class WeightbridgeRawMaterial(models.Model):
     paid_by = fields.Many2one('res.users', string='Paid By', states={'to_paid': [('required', False)], 'paid': [('required', True)], 'confirmed': [('required', False)]})
     date_paid = fields.Datetime(string='Date Paid', states={'to_paid': [('readonly', True)], 'paid': [('readonly', True)], 'confirmed': [('readonly', False)]})
     state = fields.Selection([('confirmed', 'Confirmed'),  ('to_paid', 'Paid request'), ('paid', 'Paid'), ('cancel', 'Cancel'),], string='Status', required=True, default='confirmed')
+    channel_ids = fields.Many2many('raw.material.channel', 'himaf_raw_material_channel_rel', 'raw_material_id', 'channel_id', string='Channels')
         
