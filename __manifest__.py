069c58aaa46d1fe7ff944389c57f667ca259af5b
AFFECHI Achoué Marc Hilarion <marc_affechi@hotmail.com>
2023-07-17 05:53:49 +0000

    add security file

--- __manifest__.py
+++ __manifest__.py
@@ -12,6 +12,7 @@
     "summary": "Receive and deliver products from the weight bridge",
     "depends": ["purchase", "stock"],
     "data": [
+        'security/ir.model.access.csv',
         'views/weightbridge_views.xml'
         ],
     "installable": True,
