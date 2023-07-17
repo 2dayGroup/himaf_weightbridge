094ab51c58ff683d843664e399355434fccda7ca
AFFECHI Achoué Marc Hilarion <marc_affechi@hotmail.com>
2023-07-17 05:34:52 +0000

    resolve mail.mixin error

--- models/himaf_farmer.py
+++ models/himaf_farmer.py
@@ -5,3 +5,5 @@ class HimafFarmer(models.Model):
     _name = "himaf.farmer"
     _inherit = 'res.partner'
     
+    channel_ids = fields.Many2many('farmer.channel', 'himaf_farmer_channel_rel', 'farmer_id', 'channel_id', string='Channels')
+    
