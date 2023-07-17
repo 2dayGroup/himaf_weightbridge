28d2063ec10bff5410eeaa3086ad65f0f898cc6d
AFFECHI Achoué Marc Hilarion <marc_affechi@hotmail.com>
2023-07-17 18:33:34 +0000

    add form view

--- models/weightbridge_raw_material.py
+++ models/weightbridge_raw_material.py
@@ -18,6 +18,8 @@ class WeightbridgeRawMaterial(models.Model):
     net_weight = fields.Float(string='Net Weight', required=True)
     created_by = fields.Char(string='Created By', required=True)
     authorized_by = fields.Many2one('res.users', string='Authorized By', states={'to_paid': [('required', True)], 'confirmed': [('required', False)]})
+    date_authorized = fields.Datetime(string='Date Authorized', states={'to_paid': [('readonly', True)], 'confirmed': [('readonly', True)]})
     paid_by = fields.Many2one('res.users', string='Paid By', states={'to_paid': [('required', False)], 'paid': [('required', True)], 'confirmed': [('required', False)]})
+    date_paid = fields.Datetime(string='Date Paid', states={'to_paid': [('readonly', True)], 'paid': [('readonly', True)], 'confirmed': [('readonly', False)]})
     status = fields.Selection([('confirmed', 'Confirmed'),  ('to_paid', 'Paid request'), ('paid', 'Paid'), ('cancel', 'Cancel'),], string='Status', required=True, default='confirmed')
         
